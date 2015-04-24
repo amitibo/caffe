ba38c1d44432a97c619c8d15acbc0c9139aeb1d2
amitibo <amitibo@campus.technion.ac.il>
2015-04-24 17:13:59 +0300

    Work on the backward_cpu. Not compiled not tested

--- src/caffe/layers/simple_conv_layer.cpp
+++ src/caffe/layers/simple_conv_layer.cpp
@@ -93,9 +93,17 @@ void SimpleConvolutionLayer<Dtype>::Backward_cpu(
   ) {
   const Dtype* weight = this->blobs_[0]->cpu_data();
   Dtype* weight_diff = this->blobs_[0]->mutable_cpu_diff();
+  
+  //
+  // Clear the weight diff.
+  //
   if (this->param_propagate_down_[0]) {
     caffe_set(this->blobs_[0]->count(), Dtype(0), weight_diff);
   }
+  
+  //
+  // Clear the bias diff.
+  //
   if (this->bias_term_ && this->param_propagate_down_[1]) {
     caffe_set(this->blobs_[1]->count(), Dtype(0),
         this->blobs_[1]->mutable_cpu_diff());
@@ -105,7 +113,10 @@ void SimpleConvolutionLayer<Dtype>::Backward_cpu(
     const Dtype* top_diff = top[i]->cpu_diff();
     const Dtype* bottom_data = bottom[i]->cpu_data();
     Dtype* bottom_diff = bottom[i]->mutable_cpu_diff();
+    
+    //
     // Bias gradient, if necessary.
+    //
     if (this->bias_term_ && this->param_propagate_down_[1]) {
       Dtype* bias_diff = this->blobs_[1]->mutable_cpu_diff();
       
@@ -120,9 +131,42 @@ void SimpleConvolutionLayer<Dtype>::Backward_cpu(
           }
         }
       }
-
     }
+    
     if (this->param_propagate_down_[0] || propagate_down[i]) {
+
+      //
+      // gradient w.r.t. bottom data, if necessary.
+      //
+      for (int n = 0; n < this->num_; n++) {
+        int o_g = this->num_output_ / this->group_;
+        int k_g = this->channels_ / this->group_;
+        for (int g = 0; g < this->group_; g++) {
+          int o_head = o_g * g;
+          int k_head = k_g * g;
+          for (int o = 0; o < o_g; o++) {
+            for (int k = 0; k < k_g; k++) {
+              for (int y = -this->pad_h_; y < this->height_out_ + this->pad_h_; y++) {
+                for (int x = -this->pad_w_; x < this->width_out_ + this->pad_w_; x++) {
+                  for (int p = this->kernel_h_; p --> 0 ;) {
+                    for (int q = this->kernel_w_; q --> 0 ;) {
+                      int in_y = y * this->stride_h_ - this->pad_h_ + this->kernel_h_ - p + 1;
+                      int in_x = x * this->stride_w_ - this->pad_w_ + this->kernel_w_ - q + 1;
+                      if (y >= 0 && y < this->height_out_
+                        && x >= 0 && x < this->width_out_) {
+                        bottom_diff[bottom[i]->offset(n, k + k_head, in_y, in_x)] +=
+                            top_diff[top[i]->offset(n, o +o_head, y, x)]
+                            * weight_data[this->blobs_[0]->offset(o + o_head, k, p, q)];
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+      
       for (int n = 0; n < this->num_; ++n) {
         // gradient w.r.t. weight. Note that we will accumulate diffs.
         if (this->param_propagate_down_[0]) {
