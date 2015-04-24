56da85ea200c7418746deef6044faa048660aaa1
amitibo <amitibo@campus.technion.ac.il>
2015-04-24 16:35:10 +0000

    Half of the gradient is working.

--- src/caffe/layers/simple_conv_layer.cpp
+++ src/caffe/layers/simple_conv_layer.cpp
@@ -115,6 +115,13 @@ void SimpleConvolutionLayer<Dtype>::Backward_cpu(
     Dtype* bottom_diff = bottom[i]->mutable_cpu_diff();
     
     //
+    // Clear the bottom diff.
+    //
+    if (this->param_propagate_down_[0]) {
+      caffe_set(bottom[i]->count(), Dtype(0), bottom_diff);
+    }
+
+    //
     // Bias gradient, if necessary.
     //
     if (this->bias_term_ && this->param_propagate_down_[1]) {
@@ -135,28 +142,30 @@ void SimpleConvolutionLayer<Dtype>::Backward_cpu(
     
     if (this->param_propagate_down_[0] || propagate_down[i]) {
 
-      //
-      // gradient w.r.t. bottom data, if necessary.
-      //
       for (int n = 0; n < this->num_; n++) {
-        int o_g = this->num_output_ / this->group_;
-        int k_g = this->channels_ / this->group_;
-        for (int g = 0; g < this->group_; g++) {
-          int o_head = o_g * g;
-          int k_head = k_g * g;
-          for (int o = 0; o < o_g; o++) {
-            for (int k = 0; k < k_g; k++) {
-              for (int y = -this->pad_h_; y < this->height_out_ + this->pad_h_; y++) {
-                for (int x = -this->pad_w_; x < this->width_out_ + this->pad_w_; x++) {
-                  for (int p = this->kernel_h_; p --> 0 ;) {
-                    for (int q = this->kernel_w_; q --> 0 ;) {
-                      int in_y = y * this->stride_h_ - this->pad_h_ + this->kernel_h_ - p + 1;
-                      int in_x = x * this->stride_w_ - this->pad_w_ + this->kernel_w_ - q + 1;
-                      if (y >= 0 && y < this->height_out_
-                        && x >= 0 && x < this->width_out_) {
-                        bottom_diff[bottom[i]->offset(n, k + k_head, in_y, in_x)] +=
-                            top_diff[top[i]->offset(n, o +o_head, y, x)]
-                            * weight_data[this->blobs_[0]->offset(o + o_head, k, p, q)];
+        //
+        // gradient w.r.t. bottom data, if necessary.
+        //
+        if (propagate_down[i]) {
+          int o_g = this->num_output_ / this->group_;
+          int k_g = this->channels_ / this->group_;
+          for (int g = 0; g < this->group_; g++) {
+            int o_head = o_g * g;
+            int k_head = k_g * g;
+            for (int o = 0; o < o_g; o++) {
+              for (int k = 0; k < k_g; k++) {
+                for (int y = 0; y < this->height_out_; y++) {
+                  for (int x = 0; x < this->width_out_; x++) {
+                    for (int p = 0; p < this->kernel_h_; p++) {
+                      for (int q = 0; q < this->kernel_w_; q++) {
+                        int in_y = y * this->stride_h_ - this->pad_h_ + p;
+                        int in_x = x * this->stride_w_ - this->pad_w_ + q;
+                        if (in_y >= 0 && in_y < this->height_
+                          && in_x >= 0 && in_x < this->width_) {
+                          bottom_diff[bottom[i]->offset(n, k + k_head, in_y, in_x)] +=
+                              top_diff[top[i]->offset(n, o +o_head, y, x)]
+                              * weight[this->blobs_[0]->offset(o + o_head, k, p, q)];
+                        }
                       }
                     }
                   }
@@ -165,18 +174,11 @@ void SimpleConvolutionLayer<Dtype>::Backward_cpu(
             }
           }
         }
-      }
-      
-      for (int n = 0; n < this->num_; ++n) {
+
         // gradient w.r.t. weight. Note that we will accumulate diffs.
         if (this->param_propagate_down_[0]) {
           this->weight_cpu_gemm(bottom_data + bottom[i]->offset(n),
               top_diff + top[i]->offset(n), weight_diff);
-        }
-        // gradient w.r.t. bottom data, if necessary.
-        if (propagate_down[i]) {
-          this->backward_cpu_gemm(top_diff + top[i]->offset(n), weight,
-              bottom_diff + bottom[i]->offset(n));
         }
       }
     }
